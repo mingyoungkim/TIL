01845dfa57f393fffc70342ff52f1b86f93619df
mingyoungkim <gmg04124@gmail.com>
2024-01-29 23:30:05 +0900

    spring_DI

---
+++ 10_Spring/core/src/main/java/hello/core/MemberApp.java
@@ -0,0 +1,30 @@
+package hello.core.member;
+
+import hello.core.AppConfig;
+
+import java.util.Arrays;
+
+public class MemberApp {
+    /*
+    public static void main(String[] args) {
+        MemberService memberService = new MemberServiceImpl();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("finde Member = " + findMember.getName());
+    }
+    */
+
+    public static void main(String[] args) {
+        AppConfig appConfig = new AppConfig();
+        MemberService memberService = appConfig.memberService();
+        Member member = new Member(1L, "memberA", Grade.VIP);
+        memberService.join(member);
+
+        Member findMember = memberService.findMember(1L);
+        System.out.println("member = " + member.getName());
+        System.out.println("finde Member = " + findMember.getName());
+    }
+}
